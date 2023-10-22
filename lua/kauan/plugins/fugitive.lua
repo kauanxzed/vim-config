e82dce7ab3fc1450462f01e0a67be8af5fcf48a2
kauan Lemes Valerio <ra-20000008-2@alunos.unicesumar.edu.br>
2023-10-22 16:55:36 -0300

    putting description for keymap

--- lua/kauan/plugins/fugitive.lua
+++ lua/kauan/plugins/fugitive.lua
@@ -17,18 +17,18 @@ return {
 
 				local bufnr = vim.api.nvim_get_current_buf()
 				local opts = { buffer = bufnr, remap = false }
-				vim.keymap.set("n", "<leader>p", function()
+				keymap.set("n", "<leader>p", function()
 					vim.cmd.Git("push")
-				end, opts)
+				end, { desc = "Push branch" }, opts)
 
 				-- rebase always
-				vim.keymap.set("n", "<leader>P", function()
+				keymap.set("n", "<leader>P", function()
 					vim.cmd.Git({ "pull", "--rebase" })
-				end, opts)
+				end, { desc = "Pull with rebase" }, opts)
 
 				-- NOTE: It allows me to easily set the branch i am pushing and any tracking
 				-- needed if i did not set the branch up correctly
-				vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts)
+				keymap.set("n", "<leader>t", ":Git push -u origin ", { desc = "push -u origin" }, opts)
 			end,
 		})
 	end,
