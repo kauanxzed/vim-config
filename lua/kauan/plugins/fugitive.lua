b3f0e310b05bc80fd06112d60b664c7f55c4a5c4
kauan Lemes Valerio <ra-20000008-2@alunos.unicesumar.edu.br>
2023-10-22 16:13:14 -0300

    adding push to <leader>p to fugitive buffer

--- lua/kauan/plugins/fugitive.lua
+++ lua/kauan/plugins/fugitive.lua
@@ -3,5 +3,10 @@ return {
 	config = function()
 		local keymap = vim.keymap
 		keymap.set("n", "<leader>gs", vim.cmd.Git)
+		local bufnr = vim.api.nvim_get_current_buf()
+		local opts = { buffer = bufnr, remap = false }
+		vim.keymap.set("n", "<leader>p", function()
+			vim.cmd.Git("push")
+		end, opts)
 	end,
 }
